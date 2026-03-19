ef1309222fc92a5d4775b5315ec19c770c14ae3d
Xavier Xiqués <xavier.xiques@gmail.com>
2026-03-19 19:21:39 +0100

    Define extension

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+import { registerChangeCaseCommands } from './features/change-case';
+import { registerSlugCommands } from './features/slug';
+import { registerAutoRenameTag } from './features/auto-rename-tag';
+import { registerOpenInGitHubCommands } from './features/open-in-github';
+import { registerFormatFilesCommands } from './features/format-files';
+import { registerExpandRecursivelyCommands } from './features/expand-recursively';
+
+export function activate(context: vscode.ExtensionContext) {
+  registerChangeCaseCommands(context);
+  registerSlugCommands(context);
+  registerAutoRenameTag(context);
+  registerOpenInGitHubCommands(context);
+  registerFormatFilesCommands(context);
+  registerExpandRecursivelyCommands(context);
+}
+
+export function deactivate() {}
