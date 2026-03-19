4547d485608cd1c8c5ad3f172937baab32766c5f
Xavier Xiqués <xavier.xiques@gmail.com>
2026-03-19 20:15:08 +0100

    Comment extension because exists by default in vs code

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { registerChangeCaseCommands } from './features/change-case';
 import { registerSlugCommands } from './features/slug';
-import { registerAutoRenameTag } from './features/auto-rename-tag';
+// import { registerAutoRenameTag } from './features/auto-rename-tag';
 import { registerOpenInGitHubCommands } from './features/open-in-github';
 import { registerFormatFilesCommands } from './features/format-files';
 import { registerExpandRecursivelyCommands } from './features/expand-recursively';
@@ -9,7 +9,7 @@ import { registerExpandRecursivelyCommands } from './features/expand-recursively
 export function activate(context: vscode.ExtensionContext) {
   registerChangeCaseCommands(context);
   registerSlugCommands(context);
-  registerAutoRenameTag(context);
+  // registerAutoRenameTag(context);
   registerOpenInGitHubCommands(context);
   registerFormatFilesCommands(context);
   registerExpandRecursivelyCommands(context);
