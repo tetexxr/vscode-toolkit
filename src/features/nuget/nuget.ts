d21069dea669b041790196a0ed5a285fcb44a461
Xavier Xiqués <xavier.xiques@gmail.com>
2026-03-28 12:18:09 +0100

    Add sidebar icon

--- src/features/nuget/nuget.ts
+++ src/features/nuget/nuget.ts
@@ -6,8 +6,23 @@
 import * as vscode from 'vscode';
 import { NugetPanel } from './nuget-panel';
 import { pickProjectFile } from './nuget-project-loader';
+import { NugetTreeProvider } from './nuget-tree-provider';
 
 export function registerNugetCommands(context: vscode.ExtensionContext): void {
+  // Sidebar tree view
+  const treeProvider = new NugetTreeProvider();
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider('nugetProjects', treeProvider),
+  );
+
+  // Refresh tree when workspace folders change or files are created/deleted
+  const watcher = vscode.workspace.createFileSystemWatcher('**/*.{csproj,fsproj,vbproj}');
+  context.subscriptions.push(
+    watcher,
+    watcher.onDidCreate(() => treeProvider.refresh()),
+    watcher.onDidDelete(() => treeProvider.refresh()),
+  );
+
   // Right-click on .csproj/.fsproj/.vbproj in explorer
   context.subscriptions.push(
     vscode.commands.registerCommand('toolkit.nuget.managePackages', (uri: vscode.Uri) => {
